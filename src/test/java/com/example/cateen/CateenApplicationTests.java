990ec62376f1d1fb69f81a2c8a1493a7f47e0363
suma-kundapura <sumakundapuraa@gmail.com>
2024-10-17 10:29:33 +0530

    First commit

---
+++ src/test/java/com/example/cateen/CateenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cateen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CateenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
